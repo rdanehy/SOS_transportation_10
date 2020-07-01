51604407e85e3c08534bf96425ed214dbb2f9977
Ryan Danehy <rdanehy@iu.edu>
2020-07-01 14:31:29 -0400

    HTML Template

--- pro_team/Src/app.py
+++ pro_team/Src/app.py
@@ -1,13 +1,13 @@
 # app.py
 
-from flask import Flask  # import flask
+from flask import Flask, render_template, url_for  # import flask
 
 app = Flask(__name__)  # create an app instance
 
 
-@app.route("/")  # at the end point /
-def hello():  # call method hello
-    return "Hello World!"  # which returns "hello world"
+@app.route('/')  # at the end point /
+def home():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":  # on running python app.py
