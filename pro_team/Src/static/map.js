2b446b179c6cb5d408500dbe8f6989c760d003b2
Ryan Danehy <rdanehy@iu.edu>
2020-07-08 18:11:05 -0400

    Popup window, and button addition

---
+++ pro_team/Src/static/map.js
@@ -0,0 +1,68 @@
+
+var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center:ol.proj.transform([-86.14, 39.79], 'EPSG:4326','EPSG:3857'),
+          zoom: 12,
+          minZoom: 10,
+          maxZoom: 18
+
+        })
+      });
+
+var layer = new ol.layer.Vector({
+     source: new ol.source.Vector({
+         features: [
+             new ol.Feature({
+                 geometry: new ol.geom.Point(ol.proj.fromLonLat([-86.1581, 39.7894]))
+             })
+         ]
+        })
+ });
+ map.addLayer(layer);
+
+ var container = document.getElementById('popup');
+ var content = document.getElementById('popup-content');
+ var closer = document.getElementById('popup-closer');
+
+ var overlay = new ol.Overlay({
+     element: container,
+     autoPan: true,
+     autoPanAnimation: {
+         duration: 250
+     }
+ });
+ map.addOverlay(overlay);
+
+ closer.onclick = function() {
+     overlay.setPosition(undefined);
+     closer.blur();
+     return false;
+ };
+
+  map.on('singleclick', function (event) {
+     if (map.hasFeatureAtPixel(event.pixel) === true) {
+         var coordinate = event.coordinate;
+         content.innerHTML ='Bus ID: 121<br/>Occupancy: 4<br/> <img src="./static/images/bus_schematic.png " height=" 300" width=" 900">'
+         overlay.setPosition(coordinate);
+     } else {
+         overlay.setPosition(undefined);
+         closer.blur();
+     }
+ });
+
+  // change mouse cursor when over marker
+map.on('pointermove', function(e) {
+  if (e.dragging) {
+    $(element).popover('destroy');
+    return;
+  }
+  var pixel = map.getEventPixel(e.originalEvent);
+  var hit = map.hasFeatureAtPixel(pixel);
+  map.getTarget().style.cursor = hit ? 'pointer' : '';
+});
